a1c018aabf9cf35a5d840d07fe0906f8cd98ab22
Jaya <jayamisra02@gmail.com>
2020-06-10 10:25:34 -0400

    update

---
+++ src/main/java/com/shoppingcart/cartservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.shoppingcart.cartservice.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) throws Exception {
+		
+		ResponseException responseException = new ResponseException(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(responseException,HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+}
